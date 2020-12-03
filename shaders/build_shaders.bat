cee0aa5be3307a64d47bb5c158dd79929067dc11
swerdloj <swerdloj@andrews.edu>
2020-12-02 19:10:51 -0500

    Updated readme. Fixed minor issues for initial release.

--- shaders/build_shaders.bat
+++ shaders/build_shaders.bat
@@ -1,3 +1,5 @@
+:: NOTE: Replace the path to glslc.exe with your own path
+
 C:\Development\shaderc\bin\glslc.exe ui\quad.frag -o ui\quad.frag.spv
 C:\Development\shaderc\bin\glslc.exe ui\quad.vert -o ui\quad.vert.spv
 C:\Development\shaderc\bin\glslc.exe image\texture.frag -o image\texture.frag.spv
